30883b2ad35c164757a0815988095d42e26828cf
UUuser492 <codexcepper@gmail.com>
2022-10-28 12:55:17 +0300

    House First Test Commit1

---
+++ RealEstateAgency.Tests/HouseTests.cs
@@ -0,0 +1,27 @@
+﻿using Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RealEstateAgency.Tests
+{
+    public class HouseTests
+    {
+        [Fact]
+        public void FullName()
+        {
+            //Arrange
+
+
+            string expected = "Вінницька Соборна";
+            //Act
+            var house = new House() { Number = "Будинок №1", Region = "Вінницька ", Address = new Address { Street = "Соборна" }, Price = 1000, Area = 100, NumberOfFloors = 1 };
+            string actual = house.ReturnAddress();
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
