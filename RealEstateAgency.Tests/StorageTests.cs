d102c5493155fd047cff3fcf217aa51c14554265
Daniil033 <beldan03.06@gmail.com>
2022-10-29 15:05:52 +0300

    StorageTests Prob

--- RealEstateAgency.Tests/StorageTests.cs
+++ RealEstateAgency.Tests/StorageTests.cs
@@ -10,5 +10,15 @@ namespace RealEstateAgency.Tests
 {
    public class StorageTests
     {
+        public void PricePerYear()
+        {
+            int pricePerMonth = 50;
+            int countOfMonth = 12;
+            int expected = pricePerMonth * countOfMonth;
+            Storage storage = new Storage() { Number = "Квартира №5", Region = "Дніпропетровська", Address = new Address { Street = "Східна" }, Price = 50, Area = 100 };
+            int actual = storage.PricePerYear();
+
+            Assert.Equal(expected, actual);
+        }
     }
 }
