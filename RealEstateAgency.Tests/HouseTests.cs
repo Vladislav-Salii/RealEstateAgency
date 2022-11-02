fe29e3f2fe2022bb34b36f0ec7b6815252f24651
UUuser492 <codexcepper@gmail.com>
2022-11-02 21:05:26 +0200

    Made changes as required

--- RealEstateAgency.Tests/HouseTests.cs
+++ RealEstateAgency.Tests/HouseTests.cs
@@ -16,10 +16,9 @@ namespace RealEstateAgency.Tests
             //Arrange
             var house = new House() { Number = "Будинок №2", Region = "Київська ", Address = new Address { Street = "Бальзака" }, Price = 1000, Area = 30, NumberOfFloors = 1 };
             //Act
-            house.Print();
-            string actual = house.Region;
+            string actual = house.Print();
             //Assert
-            Assert.StartsWith("Київська ", actual);
+            Assert.StartsWith("Область:Київська ", actual);
         }
 
         [Fact]
@@ -38,17 +37,25 @@ namespace RealEstateAgency.Tests
         public void TotalAreaPrice_3floors_40sqMeters()
         {
             //Arrange
-            decimal expected1 = 3694830;
-            decimal expected2 = 902723.1M;
-            var house1 = new House() { Number = "Будинок №2", Region = "Київська ", Address = new Address { Street = "Бальзака" }, Price = 1000, Area = 40, NumberOfFloors = 3 };
-            var house2 = new House() { Number = "Будинок №84A", Region = "Київська ", Address = new Address { Street = "Сержа Лифаря" }, Price = 1000, Area = 30, NumberOfFloors = 1 };
+            decimal expected = 3694830;
+            var house = new House() { Number = "Будинок №2", Region = "Київська ", Address = new Address { Street = "Бальзака" }, Price = 1000, Area = 40, NumberOfFloors = 3 };
+            //Act
+            decimal actual = house.Purchase_Price(house.Area, house.NumberOfFloors, 0);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TotalAreaPrice_1floor_30sqMeters()//separated from previous unit test
+        {
+            //Arrange
+            decimal expected = 902723.1M;
+            var house = new House() { Number = "Будинок №84A", Region = "Київська ", Address = new Address { Street = "Сержа Лифаря" }, Price = 1000, Area = 30, NumberOfFloors = 1 };
             int precision = 2;
             //Act
-            decimal actual1 = house1.Purchase_Price(house1.Area, house1.NumberOfFloors, 0);
-            decimal actual2 = house2.Purchase_Price(house2.Area, house2.NumberOfFloors, 30090.77M);
+            decimal actual = house.Purchase_Price(house.Area, house.NumberOfFloors, 30090.77M);
             //Assert
-            Assert.Equal(expected1, actual1);
-            Assert.Equal(expected2, actual2, precision);
+            Assert.Equal(expected, actual, precision);
         }
 
         [Fact]
@@ -56,7 +63,7 @@ namespace RealEstateAgency.Tests
         {
             //Arrange
             var purchased_house = new House() { Number = "Будинок №2", Region = "Київська ", Address = new Address { Street = "Бальзака" }, Price = 1000, Area = 30, NumberOfFloors = 1 };
-            decimal price = purchased_house.Purchase_Price(purchased_house.Area, purchased_house.NumberOfFloors, 30090.77M);
+            decimal price = 902723.1M;
             //Act
             decimal actual = purchased_house.RieltorPart(price, 0.05M);
             //Assert
@@ -68,9 +75,8 @@ namespace RealEstateAgency.Tests
         {
             //Arrange
             decimal RieltorPart = 0.05M;
-            decimal PricePerMeter = 30090.77M;
             var purchased_house = new House() { Number = "Будинок №2", Region = "Київська ", Address = new Address { Street = "Бальзака" }, Price = 1000, Area = 30, NumberOfFloors = 1 };
-            decimal price = purchased_house.Purchase_Price(purchased_house.Area, purchased_house.NumberOfFloors, PricePerMeter);
+            decimal price = 902723.1M;
             decimal expected = 947859.255M;
             //Act
             decimal actual = purchased_house.RealPrice(price, RieltorPart);
@@ -82,12 +88,10 @@ namespace RealEstateAgency.Tests
         public void Purchasing_Condition()
         {
             //Arrange
-            decimal RieltorPart = 0.05M;
             var purchasing_house = new House() { Number = "Будинок №2", Region = "Київська ", Address = new Address { Street = "Бальзака" }, Price = 1000, Area = 30, NumberOfFloors = 1 };
-            decimal PricePerMeter = 30090.77M;
-            decimal price = purchasing_house.Purchase_Price(purchasing_house.Area, purchasing_house.NumberOfFloors, PricePerMeter);
+            decimal price = 902723.1M;
             purchasing_house.DocumentsAreVerified = true;
-            decimal RealPrice = purchasing_house.RealPrice(price, RieltorPart);
+            decimal RealPrice = 902723.1M + 902723.1M * 0.05M;
             //Act
             bool actual = purchasing_house.Purchasing(purchasing_house.DocumentsAreVerified, price, RealPrice);
             //Assert
