b11a7c2fc054a92fdd80d14e7fd3f078038adf61
vladislav.salii@gmail.com <vladislav.salii@gmail.com>
2022-11-08 10:33:50 +0200

    Check Filter

--- RealEstateAgency.Tests/FlatTests.cs
+++ RealEstateAgency.Tests/FlatTests.cs
@@ -1,4 +1,5 @@
 ﻿using Design;
+using System;
 using Xunit;
 
 namespace RealEstateAgency.Tests
@@ -59,7 +60,6 @@ namespace RealEstateAgency.Tests
         [InlineData(12)]
         [InlineData(14)]
         [InlineData(10)]
-        [InlineData(46)]
         public void Deposit_ShouldBeInRangeOf10to15Percent(object value)
         {
 
@@ -72,15 +72,119 @@ namespace RealEstateAgency.Tests
 
         }
 
+        [Theory]
+        [InlineData(46)]
+        [InlineData(20)]
+        public void DepositShouldBeInRangeOf10to15Percent_ReturnMinus1(int value)
+        {
+
+            //act
+            Flat flat = new Flat() { Number = "Квартира №1", Region = "Вінницька", Address = new Address { Street = "Лесі Українки" }, Price = 100, Area = 100, CountOfRoom = 3 };
+            int actual = flat.CheckDeposit((int)value);
+
+            //excepted
+            int excepted = -1;
+
+            //assert
+            Assert.Equal(excepted, actual);
+
+        }
+
+
         [Fact]
-        public void State_HaveFree()
+        public void CheckFilterOnNull_ReturnFalse()
         {
             //Arrange
-            var sut = new Flat();
-            string expected = "Free";
+            Flat flatform = new Flat() { Number = "Квартира №1", Region = "", Address = new Address { Street = "Лесі Українки" }, Price = 80, Area = 0, CountOfRoom = 0 };
+            Flat flatthis = new Flat() { Number = "Квартира №3", Region = "", Address = new Address { Street = "Лесі Українки" }, Price = 100, Area = 0, CountOfRoom = 0 };
+            bool actual = flatthis.Filter(null, flatform);
+
+            //expected
+            bool expected = false;
 
             //Assert
-            Assert.Contains(expected, sut.State);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CheckFilterOnCastingTypes_ReturnFalse()
+        {
+            //Arrange
+            Flat flatform = new Flat() { Number = "Квартира №1", Region = "", Address = new Address { Street = "Лесі Українки" }, Price = 80, Area = 0, CountOfRoom = 0 };
+            Flat flatthis = new Flat() { Number = "Квартира №3", Region = "", Address = new Address { Street = "Лесі Українки" }, Price = 100, Area = 0, CountOfRoom = 0 };
+            bool actual = flatthis.Filter(new DateTime(), flatform);
+
+            //expected
+            bool expected = false;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CheckFilterOnPrice_PriceMore80AndLess120_ReturnTrue()
+        {
+            //Arrange
+            Flat flatform = new Flat() { Number = "Квартира №1", Region = "", Address = new Address { Street = "Лесі Українки" }, Price = 80, Area = 0, CountOfRoom = 0 };
+            Flat flatto= new Flat() { Number = "Квартира №2", Region = "", Address = new Address { Street = "Лесі Українки" }, Price = 125, Area = 0, CountOfRoom = 0 };
+            Flat flatthis = new Flat() { Number = "Квартира №3", Region = "", Address = new Address { Street = "Лесі Українки" }, Price = 100, Area = 0, CountOfRoom = 0 };
+            bool actual = flatthis.Filter(flatform, flatto);
+
+            //expected
+            bool expected = true;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void CheckFilterOnArea_AreaMore75AndLess150_ReturnTrue()
+        {
+            //Arrange
+            Flat flatform = new Flat() { Number = "Квартира №1", Region = "", Address = new Address { Street = "Лесі Українки" }, Price = 0, Area = 75, CountOfRoom = 0 };
+            Flat flatto = new Flat() { Number = "Квартира №2", Region = "", Address = new Address { Street = "Лесі Українки" }, Price = 0, Area = 150, CountOfRoom = 0 };
+            Flat flatthis = new Flat() { Number = "Квартира №3", Region = "", Address = new Address { Street = "Лесі Українки" }, Price = 0, Area = 100, CountOfRoom = 0 };
+            bool actual = flatthis.Filter(flatform, flatto);
+
+            //expected
+            bool expected = true;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CheckFilterOnCountOfRoom_CountOfRoomMore1AndLess4_ReturnTrue()
+        {
+            //Arrange
+            Flat flatform = new Flat() { Number = "Квартира №1", Region = "", Address = new Address { Street = "Лесі Українки" }, Price = 0, Area = 0, CountOfRoom = 1 };
+            Flat flatto = new Flat() { Number = "Квартира №2", Region = "", Address = new Address { Street = "Лесі Українки" }, Price = 0, Area = 0, CountOfRoom = 4 };
+            Flat flatthis = new Flat() { Number = "Квартира №3", Region = "", Address = new Address { Street = "Лесі Українки" }, Price = 0, Area = 0, CountOfRoom = 2 };
+            bool actual = flatthis.Filter(flatform, flatto);
+
+            //expected
+            bool expected = true;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void CheckFilterOnRegion()
+        {
+            //Arrange
+            Flat flatform = new Flat() { Number = "Квартира №1", Region = "Вінниця", Address = new Address { Street = "Лесі Українки" }, Price = 0, Area = 0, CountOfRoom = 0 };
+            Flat flatto = new Flat() { Number = "Квартира №2", Region = "Київ", Address = new Address { Street = "Лесі Українки" }, Price = 0, Area = 0, CountOfRoom = 0 };
+            Flat flatthis = new Flat() { Number = "Квартира №3", Region = "Хмельницький", Address = new Address { Street = "Лесі Українки" }, Price = 0, Area = 0, CountOfRoom = 0 };
+            bool actual = flatthis.Filter(flatform, flatto);
+
+            //expected
+            bool expected = false;
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
 
     }
