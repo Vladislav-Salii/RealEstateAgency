12e02848fde650bc1f06d2b3d8fc6dd53613cbd7
Daniil033 <beldan03.06@gmail.com>
2022-11-10 15:20:13 +0200

    T0TM-81 Belyi Daniil

--- RealEstateAgency.Tests/StorageTests.cs
+++ RealEstateAgency.Tests/StorageTests.cs
@@ -11,6 +11,18 @@ namespace RealEstateAgency.Tests
    public class StorageTests
     {
         [Fact]
+        public void StoragePrint()
+        {
+            string expected = "Номер будинку : 1 , Область : Дніпропетровська , Вулиця : Соборна , Ціна : 100 , Площа : 100";
+
+            Storage storage = new Storage() { Number = "1", Region = "Дніпропетровська", Address = new Address { Street = "Соборна" }, Price = 100, Area = 100 };
+            string actual = storage.Print();
+
+            
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
         public void PricePerYear()
         {
             int pricePerMonth = 50;
@@ -45,6 +57,70 @@ namespace RealEstateAgency.Tests
 
             Storage storage = new Storage() { Number = "8", Region = "ДніпроКиїв", Address = new Address { Street = "Соборна" }, Price = 50, Area = 200 };
             string actual = storage.OurRegion();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FilterAtNull_BoolCheckReturnFalse()
+        {
+            Storage storageform = new Storage() { Number = "1", Region = "", Address = new Address { Street = "Східна" }, Price = 80, Area = 0 };
+            Storage storagethis = new Storage() { Number = "3", Region = "", Address = new Address { Street = "Східна" }, Price = 100, Area = 0 };
+            bool actual = storagethis.Filter(null, storageform);
+
+            bool expected = false;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FilterPriceAt80To120_BoolCheckReturnTrue()
+        {
+            Storage storageform = new Storage() { Number = "1", Region = "", Address = new Address { Street = "Соборна" }, Price = 70, Area = 0 };
+            Storage storageto = new Storage() { Number = "2", Region = "", Address = new Address { Street = "Соборна" }, Price = 120, Area = 0 };
+            Storage storagethis = new Storage() { Number = "3", Region = "", Address = new Address { Street = "Соборна" }, Price = 100, Area = 0 };
+            bool actual = storagethis.Filter(storageform, storageto);
+
+            bool expected = true;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FilterAreaAt90To130_BoolCheckReturnTrue()
+        {
+
+            Storage storageform = new Storage() { Number = "1", Region = "", Address = new Address { Street = "Соборна" }, Price = 0, Area = 90 };
+            Storage storageto = new Storage() { Number = "2", Region = "", Address = new Address { Street = "Соборна" }, Price = 0, Area = 130 };
+            Storage storagethis = new Storage() { Number = "3", Region = "", Address = new Address { Street = "Соборна" }, Price = 0, Area = 100 };
+            bool actual = storagethis.Filter(storageform, storageto);
+
+            bool expected = true;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FilterCastingTypes_BoolCheckReturnTrue()
+        {
+            Storage storageform = new Storage() { Number = "1", Region = "", Address = new Address { Street = "Соборна" }, Price = 70, Area = 0 };
+            Storage storagethis = new Storage() { Number = "2", Region = "", Address = new Address { Street = "Соборна" }, Price = 90, Area = 0 };
+            bool actual = storagethis.Filter(new DateTime(), storageform);
+
+            bool expected = false;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FilterRegion_BoolCheckReturnTrue()
+        {
+            Storage storageform = new Storage() { Number = "1", Region = "Дніпро", Address = new Address { Street = "Соборна" }, Price = 0, Area = 0 };
+            Storage storageto = new Storage() { Number = "2", Region = "Дніпро", Address = new Address { Street = "Соборна" }, Price = 0, Area = 0 };
+            Storage storagethis = new Storage() { Number = "3", Region = "Дніпро", Address = new Address { Street = "Соборна" }, Price = 0, Area = 0 };
+            bool actual = storagethis.Filter(storageform, storageto);
+
+            bool expected = true;
 
             Assert.Equal(expected, actual);
         }
